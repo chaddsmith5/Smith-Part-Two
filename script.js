3753ccffb26e7220da694979a6bdbf7cfb7bbefd
Chadd <chaddsmith5@gmail.com>
2019-03-21 15:56:07 -0400

    revision of script 3/21

--- script.js
+++ script.js
@@ -3,22 +3,22 @@
 
     var recommendedClothing
     if( EventType == "semi-formal" ) {
-       recommendedClothing = "Wear a polo, ";
+       recommendedClothing = "Wear a polo and ";
     } else if( EventType == "casual" ) {
-       recommendedClothing = "Wear something comfy, ";
+       recommendedClothing = "Wear something comfy but, ";
     } else if( EventType == "formal" ) {
-       recommendedClothing = "Wear a suit, ";
+       recommendedClothing = "Wear a suit and ";
     } else {
        recommendedClothing = "Wear nothing!";
     }
 
     var weatherAssessment
-    if( tempFahr <= 70 ) {
-       weatherAssessment = "it's hot outside!";
-    } else if( tempFahr >= 54 ) {
-       weatherAssessment = "it's gonna be chilly outside!";
+    if( tempFahr >= 70 ) {
+       weatherAssessment = "forget the jacket, it's hot outside!";
+    } else if( tempFahr <= 54 ) {
+       weatherAssessment = "grab a coat, it's gonna be chilly outside!";
     } else if( tempFahr < 54 || tempFahr > 70 ) {
-       weatherAssessment = "it's pleasant outside!";
+       weatherAssessment = "maybe a jacket, it's pleasant outside!";
     } else {
        weatherAssessment = "Who cares about the weather,";
     }
